c10f6c7e25899bc8396b8784501df65cff306de4
Jagailo <Jagailo96@gmail.com>
2018-02-18 20:13:01 +0300

    Fixed tabs.

--- js/logic.js
+++ js/logic.js
@@ -1,19 +1,19 @@
 function randomInteger(min, max) {
-	var rand = min - 0.5 + Math.random() * (max - min + 1);
-	return Math.round(rand);
+    var rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
 }
 
 function getConverArray(array) {
-	for (var i = 0; i < array.length; i++) {
-		array[i] = +array[i];
-	}
-	return array;
+    for (var i = 0; i < array.length; i++) {
+        array[i] = +array[i];
+    }
+    return array;
 }
 
 function printArray(array, TagID) {
-	var str = '';
-	for (var i = 0; i < array.length; i++) {
-		str += '<span class="array-elem-cell">' + array[i] + '</span>';
-	}
-	document.getElementById(TagID).innerHTML = str;
+    var str = '';
+    for (var i = 0; i < array.length; i++) {
+        str += '<span class="array-elem-cell">' + array[i] + '</span>';
+    }
+    document.getElementById(TagID).innerHTML = str;
 }
--- js/main.js
+++ js/main.js
@@ -3,29 +3,29 @@ var randomArrayIntRange = [-99, 99] // [min, max]
 var sourceArray;
 
 function fillArray() {
-	var inputString = document.getElementById('arrayInput').value;	
-	var inputArray = inputString.match(/-?\d+(?:\.\d+)?/g);
-	
-	if (inputArray != null) {
-		sourceArray = getConverArray(inputArray);
-		printArray(sourceArray, 'sourceArray');
-	}
+    var inputString = document.getElementById('arrayInput').value;
+    var inputArray = inputString.match(/-?\d+(?:\.\d+)?/g);
+    
+    if (inputArray != null) {
+        sourceArray = getConverArray(inputArray);
+        printArray(sourceArray, 'sourceArray');
+    }
 }
 
 function randomFill() {
-	var string = '';
-	var arraySize = randomInteger(randomArraySizeRange[0], randomArraySizeRange[1]);
-	
-	for (var i = 0; i < arraySize; i++) {
-		string += randomInteger(randomArrayIntRange[0], randomArrayIntRange[1]) + ' ';
-	}
-	
-	document.getElementById('arrayInput').value = string;
-	fillArray();
+    var string = '';
+    var arraySize = randomInteger(randomArraySizeRange[0], randomArraySizeRange[1]);
+    
+    for (var i = 0; i < arraySize; i++) {
+        string += randomInteger(randomArrayIntRange[0], randomArrayIntRange[1]) + ' ';
+    }
+    
+    document.getElementById('arrayInput').value = string;
+    fillArray();
 }
 
 function sortArray() {
-	if (sourceArray != null) {
-		printArray(quickSort(sourceArray), 'currentArray');
-	}
+    if (sourceArray != null) {
+        printArray(quickSort(sourceArray), 'currentArray');
+    }
 }
